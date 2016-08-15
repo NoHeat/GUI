16db9d76f41e823e0db80f04c8374c97a942ac24
Reynaldo Rodriguez <reyronald19@hotmail.com>
2016-08-15 02:06:17 -0400

    Signed-off-by: Reynaldo Rodriguez <reyronald19@hotmail.com>

--- welcomescreen.cpp
+++ welcomescreen.cpp
@@ -16,6 +16,9 @@ WelcomeScreen::~WelcomeScreen()
 
 void WelcomeScreen::on_WelcomeScreenStartButton_clicked()
 {
+    /*On top is the method that gives functionality to the start button in the first window,
+     * upon clicking it an instance of the second window is created. Setting Modal to true means
+     * the user can only interact with that window when they click the button. */
     ActionMenu myActionMenu;
     myActionMenu.setModal(true);
     myActionMenu.exec();
