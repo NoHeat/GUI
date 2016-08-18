aa58c4c85732c694ddc95f1888b2e235f42ac7bd
Reynaldo Rodriguez <Reyronald19@hotmail.com>
2016-08-18 08:55:29 -0400

    Updated some buttons to contain images

--- custominput.cpp
+++ custominput.cpp
@@ -8,13 +8,24 @@ CustomInput::CustomInput(QWidget *parent) :
     ui(new Ui::CustomInput)
 {
     ui->setupUi(this);
-    QPixmap pixFruits (":/ImagesAndIcons/fru_fin.png");
-    ui->labelFruit->setPixmap(pixFruits);
-    ui->labelFruit->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+    //QPixmap pixFruits (":/ImagesAndIcons/fru_fin.png");
+    //ui->labelFruit->setPixmap(pixFruits);
+   // ui->labelFruit->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 
-    QPixmap pixVegetables (":/ImagesAndIcons/vegg_fin.png");
-    ui->labelVegetable->setPixmap(pixVegetables);
-    ui->labelVegetable->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+    //QPixmap pixVegetables (":/ImagesAndIcons/vegg_fin.png");
+    //ui->labelVegetable->setPixmap(pixVegetables);
+    //ui->labelVegetable->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+
+        //This is an example of how to make a button clickable with an image
+        QPixmap pixmap (":/ImagesAndIcons/fru_fin.png");
+        QIcon ButtonIconFruit(pixmap);
+        ui->btn_fruits_input->setIcon(ButtonIconFruit);
+        ui->btn_fruits_input->setIconSize(pixmap.rect().size());
+
+        QPixmap pixmap1 (":/ImagesAndIcons/vegg_fin.png");
+        QIcon ButtonIconVegetable(pixmap1);
+        ui->btn_vegetables_input->setIcon(ButtonIconVegetable);
+        ui->btn_vegetables_input->setIconSize(pixmap.rect().size());
 
 }
 
